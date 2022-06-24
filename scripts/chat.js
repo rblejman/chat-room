22e5da7ae12d12ba21f238f136c7a8caa406341a
rodrigo blejman <rodrigo.blej@gmail.com>
2022-06-24 17:13:25 -0300

    chatroom object initialized. firestore connection OK

---
+++ scripts/chat.js
@@ -0,0 +1,26 @@
+class Chatroom {
+  constructor(room, username) {
+    this.room = room;
+    this.username = username;
+    this.chats = db.collection("chats");
+  }
+  async addChat(message) {
+    const now = new Date();
+    const chat = {
+      message: message,
+      username: this.username,
+      room: this.room,
+      created_at: firebase.firestore.Timestamp.fromDate(now),
+    };
+    const response = await this.chats.add(chat);
+    return response;
+  }
+}
+
+const chatroom = new Chatroom("gaming", "rodrigo");
+chatroom
+  .addChat("hola a todos")
+  .then(() => {
+    console.log("chat added");
+  })
+  .catch((err) => console.log(err));
