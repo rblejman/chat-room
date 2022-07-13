13ba025d57482290a02f105fcdc3d50d7685cda2
rodrigo blejman <rodrigo.blej@gmail.com>
2022-07-13 19:44:44 -0300

    name updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 // dom queries
 const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
+const newNameForm = document.querySelector(".new-name");
+const updateMssg = document.querySelector(".update-mssg");
 
 //add a new chat
 newChatForm.addEventListener("submit", (e) => {
@@ -14,6 +16,18 @@ newChatForm.addEventListener("submit", (e) => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+// update username
+newNameForm.addEventListener("submite", (e) => {
+  e.preventDefault();
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  newNameForm.reset();
+  updateMssg.innerText = `Your name was updated to ${newName}`;
+  setTimeout(() => {
+    updateMssg.innerText = "";
+  }, 3000);
 });
 
 // class instances
